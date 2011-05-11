eef4c7c48b698ee6719823fbf530f97bbe85cf1b
cdmarchionne@gmail.com <cdmarchionne@gmail.com@e7c93e61-3541-23d9-af5e-aafccc783727>
2011-05-11 00:16:27 +0000

    Genero 4 equipos automaticamente

--- persist-ball/src/main/java/ar/edu/unq/tpi/persistencia/utils/ListUtils.java
+++ persist-ball/src/main/java/ar/edu/unq/tpi/persistencia/utils/ListUtils.java
@@ -1,34 +1,62 @@
 package ar.edu.unq.tpi.persistencia.utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ar.edu.unq.tpi.persistencia.bean.Titular;
+import ar.edu.unq.tpi.persistencia.enums.Posicion;
 import ar.edu.unq.tpi.persistencia.logic.Formacion;
 
 public class ListUtils {
-	
-	public static boolean compareLists(List<?> l1, List<?> l2){				
-		return l1.containsAll(l2) && l1.size()== l2.size();
-	}
-	
-	
-	public static Object random(List<?> list){	
-		return random(list.toArray());
-	}
-	
-	public static Object random(Object... objects){
-		return objects[(int) (Math.random()*objects.length)];
-	}
-	
-	
-	
-	public static boolean titularesNoEnSuplentes(Formacion aFormation){
 
-		for(Titular t: aFormation.getTitulares()){
-			if (aFormation.getSuplentes().contains(t))
-				return false;
-		}
+    public static boolean compareLists(final List<?> l1, final List<?> l2) {
+        return l1.containsAll(l2) && l1.size() == l2.size();
+    }
 
-		return true;
-	}
+    public static Object random(final List<?> list) {
+        return random(list.toArray());
+    }
+
+    public static Object random(final Object... objects) {
+        return objects[(int) (Math.random() * objects.length)];
+    }
+
+    public static List<Posicion> posicionRandom() {
+        List<Posicion> posiciones = new ArrayList<Posicion>();
+
+        List<Posicion> posicionesBasicas = new ArrayList<Posicion>();
+        // Agrego las posiciones minimas que debo cubrir para formar un equipo
+        posicionesBasicas.add(Posicion.ARQUERO);
+        posicionesBasicas.add(Posicion.LATERAL);
+        posicionesBasicas.add(Posicion.CENTRAL);
+        posicionesBasicas.add(Posicion.VOLANTE_DEFENSIVO);
+        posicionesBasicas.add(Posicion.VOLANTE_LATERAL);
+        posicionesBasicas.add(Posicion.ENGANCHE);
+        posicionesBasicas.add(Posicion.MEDIA_PUNTA);
+        posicionesBasicas.add(Posicion.DELANTERO);
+
+        posiciones.addAll(posicionesBasicas);
+
+        // Completo el equipo aleatoreamente
+        Posicion posicionesAleatoreas;
+        while (posiciones.size() < 11) {
+            posicionesAleatoreas = (Posicion) random(posicionesBasicas);
+            if (posicionesAleatoreas != Posicion.ARQUERO) {
+                posiciones.add(posicionesAleatoreas);
+            }
+        }
+
+        return posiciones;
+    }
+
+    public static boolean titularesNoEnSuplentes(final Formacion aFormation) {
+
+        for (Titular t : aFormation.getTitulares()) {
+            if (aFormation.getSuplentes().contains(t)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
